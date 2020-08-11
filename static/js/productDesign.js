4752dda6e902c01c6b50c6fc7f5a847042c4a22f
Dana BB <danabenbasa@gmail.com>
2020-08-11 13:19:34 +0300

    playing with fabric

--- static/js/productDesign.js
+++ static/js/productDesign.js
@@ -1,26 +1,26 @@
 let canvas = new fabric.Canvas('product-canvas');
 
-function updateTshirtImage(imageURL){
+function updateProductImage(imageURL){
     fabric.Image.fromURL(imageURL, function(img) {
-        img.scaleToHeight(300);
-        img.scaleToWidth(300);
+        img.scaleToHeight(150);
+        img.scaleToWidth(150);
         canvas.centerObject(img);
         canvas.add(img);
         canvas.renderAll();
     });
 }
 
-// Update the TShirt color according to the selected color by the user
+// Update the Product color according to the selected color by the user
 document.getElementById("product-color").addEventListener("change", function(){
     document.getElementById("product-div").style.backgroundColor = this.value;
 }, false);
 
-// Update the TShirt color according to the selected color by the user
+// Update the Product color according to the selected color by the user
 document.getElementById("product-design").addEventListener("change", function(){
 
-    // Call the updateTshirtImage method providing as first argument the URL
+    // Call the updateProductImage method providing as first argument the URL
     // of the image provided by the select
-    updateTshirtImage(this.value);
+    updateProductImage(this.value);
 }, false);
 
 // When the user clicks on upload a custom picture
@@ -35,8 +35,8 @@ document.getElementById('product-custompicture').addEventListener("change", func
         imgObj.onload = function () {
             var img = new fabric.Image(imgObj);
 
-            img.scaleToHeight(300);
-            img.scaleToWidth(300);
+            img.scaleToHeight(100);
+            img.scaleToWidth(100);
             canvas.centerObject(img);
             canvas.add(img);
             canvas.renderAll();
@@ -62,16 +62,49 @@ document.addEventListener("keydown", function(e) {
 
 
 //Todo: make the add to cart open only after done editing and make done editing replace the shirt editor with a photo
-function drawImg(){
+function doneEdit(){
     // Define as node the T-Shirt Div
     let node = document.getElementById('product-div');
     domtoimage.toPng(node).then(function (dataUrl) {
         // Print the data URL of the picture in the Console
         document.getElementById('productWithImage').value = dataUrl
         document.getElementById('addToCart').disabled = false;
+        document.getElementById('continueEdit').disabled = false;
+        document.getElementById('doneEdit').disabled = true;
+        document.getElementsByClassName('upper-canvas').removeEventListener("mousedown","_onMouseDown");
+        //console.log("value = :" + document.getElementById('shirtWithImage').value)
+    }).catch(function (error) {
+        console.error('oops, something went wrong!', error);
+    });
+}
+
+function continueEdit(){
+    // Define as node the T-Shirt Div
+    let node = document.getElementById('product-div');
+    domtoimage.toPng(node).then(function (dataUrl) {
+        // Print the data URL of the picture in the Console
+        document.getElementById('productWithImage').value = dataUrl
+        document.getElementById('addToCart').disabled = false;
+        document.getElementById('continueEdit').disabled = false;
+        document.getElementById('doneEdit').disabled = true;
 
         //console.log("value = :" + document.getElementById('shirtWithImage').value)
     }).catch(function (error) {
         console.error('oops, something went wrong!', error);
     });
 }
+/*document.addEventListener("mousedown", function(t) {
+    fabric.__onMouseDown(t), fabric._resetTransformEventData();
+    var e = fabric.upperCanvasEl, i = fabric._getEventPrefix();
+    fabric.n(e, i + "move", this._onMouseMove, fabric.s), fabric.r(fabric.document, i + "up", fabric._onMouseUp), fabric.r(fabric.document, i + "move", fabric._onMouseMove, fabric.s)
+});*/
+/* _onMouseDown: function (t) {
+    /*todo: this.__onMouseDown(t), this._resetTransformEventData();
+    var e = this.upperCanvasEl, i = this._getEventPrefix();
+    n(e, i + "move", this._onMouseMove, s), r(fabric.document, i + "up", this._onMouseUp), r(fabric.document, i + "move", this._onMouseMove, s)
+}*/
+canvas.on('mouse:over', function(e) {
+    e.target.set('fill', 'red');
+    canvas.renderAll();
+});
+
